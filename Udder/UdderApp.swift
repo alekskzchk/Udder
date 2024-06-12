dfa4769698ba46d53003e1e429c7f86e1f08379b
Alexey Kozachuk <leha.koza4uk@ya.ru>
2024-06-12 15:42:51 +1000

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Алексей Козачук on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
